7c2a285d9f7b36f1712b9d3823282b4f25821e71
bookofjunaid <u2744164@uel.ac.uk>
2025-11-12 12:30:33 +0000

    week7

--- week7/week7mj/src/index.js
+++ week7/week7mj/src/index.js
@@ -4,12 +4,13 @@ import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 import Hook_ControlledButtonState from'./counter'
-import EmojeeCounter from './EmojeeCounters'
+import EmojeeCounter from './EmojeeCounter'
+
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <Hook_ControlledButtonState />
+    <Hook_ControlledButtonState/>
     <EmojeeCounter pic='Love'/>
 <EmojeeCounter pic='sad'/>
 <EmojeeCounter pic='Like'/>
